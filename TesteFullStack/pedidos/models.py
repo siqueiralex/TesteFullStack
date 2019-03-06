8e7ac3eac87ea11d2b9e4966f1908d9618b65277
Alex Siqueira Lacerda <Lequizi@gmail.com>
2019-03-05 23:04:43 -0300

    adding Produtos model

--- TesteFullStack/pedidos/models.py
+++ TesteFullStack/pedidos/models.py
@@ -7,3 +7,11 @@ class Cliente(models.Model):
     
     def __str__(self):
         return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.FloatField()
+    multiplo = models.SmallIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
