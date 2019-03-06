21d5eba341c7a0e4db09d389edcb4b1d7aeffa66
Alex Siqueira Lacerda <Lequizi@gmail.com>
2019-03-05 23:18:23 -0300

    configuring admin

--- TesteFullStack/pedidos/admin.py
+++ TesteFullStack/pedidos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cliente, Produto, Pedido, ItemPedido
 
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Produto)
+admin.site.register(Pedido)
+admin.site.register(ItemPedido)
