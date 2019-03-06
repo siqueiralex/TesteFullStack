9fe85244f0e44763db271a2d34c08f1919e0ae32
Alex Siqueira Lacerda <Lequizi@gmail.com>
2019-03-06 19:35:35 -0300

    adding home view

--- TesteFullStack/pedidos/views.py
+++ TesteFullStack/pedidos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Pedido
 
 # Create your views here.
+
+def home(request):
+    pedidos = Pedido.objects.all()
+    return render(request, 'home.html', {'pedidos': pedidos})
