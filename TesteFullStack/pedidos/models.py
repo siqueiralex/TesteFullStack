933f697992c1da9b0bb8b3c9e2d5995ce392379e
Alex Siqueira Lacerda <Lequizi@gmail.com>
2019-03-05 23:07:41 -0300

    adding Pedido model

--- TesteFullStack/pedidos/models.py
+++ TesteFullStack/pedidos/models.py
@@ -15,3 +15,19 @@ class Produto(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, related_name='cliente', on_delete=models.CASCADE)
+    produtos = models.ManyToManyField(Produto, through='ItemPedido', blank=True, related_name='produtos')
+     
+    def __str__(self):
+        return self.cliente.nome + "#"+str(self.id)
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, related_name = "pertence", on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, related_name="pertence", on_delete=models.CASCADE)
+    quantidade = models.SmallIntegerField()
+    preco = models.FloatField()
+    
+    def __str__(self):
+        return str(self.pedido)+"_"+str(self.produto)
